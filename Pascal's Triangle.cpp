20f5d29ed0d8451bd08a6f2d665ee53057e38152
Aryan <90371726+Idiot-Coder@users.noreply.github.com>
2021-09-20 21:46:03 +0530

    11

---
+++ Pascal's Triangle.cpp
@@ -0,0 +1,17 @@
+//Program to Pascal's Triangle
+//Created by Aryan
+//Created on 20/09/2021
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> r(numRows);
+        for(int i=0;i<numRows;i++){
+            r[i].resize(i+1);
+            r[i][0]=r[i][i]=1;
+            for(int j=1;j<i;j++)
+                r[i][j]=r[i-1][j-1]+r[i-1][j];
+        }
+        return r;
+    }
+};
