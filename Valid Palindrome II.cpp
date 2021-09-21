94ccf7d962967092e1a2a6627ef236349f14701e
Aryan <90371726+Idiot-Coder@users.noreply.github.com>
2021-09-21 12:55:22 +0530

    15

---
+++ Valid Palindrome II.cpp
@@ -0,0 +1,33 @@
+//Program to Valid Palindrome II
+//Created by Aryan
+//Created on 21/09/2021
+
+class Solution {
+public:
+      bool validPalindrome(string s) {
+         int len = s.size(), i = 0, j = len - 1;
+	while (i < j) {
+		if (s[i] == s[j]) {
+			++i;
+			--j;
+		}
+		else {
+		return f(s, i + 1, j) || f(s, i, j - 1);
+		}
+	}
+	return true;
+	}
+private:
+       bool f(const string & s, int i, int j) {
+	while (i < j) {
+		if (s[i] == s[j]) {
+			++i;
+			--j;
+		}
+		else {
+		return false;
+		}
+	}
+	return true;
+	}
+};
