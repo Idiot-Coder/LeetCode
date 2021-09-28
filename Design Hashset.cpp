da9389a6cf59741acb6a42a67bc52ceb34704586
Aryan <90371726+Idiot-Coder@users.noreply.github.com>
2021-09-28 07:40:28 +0530

    35

---
+++ Design Hashset.cpp
@@ -0,0 +1,30 @@
+//Program to Design Hashset
+//Created by Aryan
+//Created on 28/09/2021
+
+class MyHashSet {
+public:
+    /** Initialize your data structure here. */
+    vector<int> v;
+    
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        if(!contains(key)){
+            v.push_back(key);
+        }
+    }
+    
+    void remove(int key) {
+        if(contains(key)){
+            v.erase(find(v.begin(), v.end(), key));
+        }
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        return find(v.begin(), v.end(), key)!=v.end();
+    }
+};
