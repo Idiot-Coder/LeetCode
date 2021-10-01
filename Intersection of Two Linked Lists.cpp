ec0f8d9d4dc191f70f8dfbb08edb9a46fdbc8d61
Aryan <90371726+Idiot-Coder@users.noreply.github.com>
2021-10-01 09:32:08 +0530

    40

---
+++ Intersection of Two Linked Lists.cpp
@@ -0,0 +1,15 @@
+//Program to Intersection of Two Linked Lists
+//Created by Aryan
+//Created on 01/10/2021
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *curA = headA, *curB = headB;
+        while (curA != curB) {
+            curA = curA ? curA->next : headB;
+            curB = curB ? curB->next : headA;
+        }
+        return curA;
+    }
+};
