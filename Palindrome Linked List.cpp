144d0d65a2b0dc6ecf3e0b3fdc0d20d70badbead
Aryan <90371726+Idiot-Coder@users.noreply.github.com>
2021-10-03 12:39:16 +0530

    42

---
+++ Palindrome Linked List.cpp
@@ -0,0 +1,19 @@
+//Program to Palindrome Linked List
+//Created by Aryan
+//Created on 03/10/2021
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> v;
+        ListNode* cur = head;
+        while(cur){
+            v.push_back(cur->val);
+            cur = cur->next;
+        }
+        for(int i = 0; i < v.size()/2; i++){
+            if(v[i] != v[v.size()-1-i]) return false;
+        }
+        return true;
+    }
+};
