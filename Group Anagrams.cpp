cbd2d12c8c9183823b0dd3d8b34ae219711e374c
Aryan <90371726+Idiot-Coder@users.noreply.github.com>
2021-11-04 09:16:27 +0530

    77

---
+++ Group Anagrams.cpp
@@ -0,0 +1,29 @@
+//Program to Group Anagrams
+//Created by Aryan
+//Created on 04/11/2021
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        string tmp;
+        vector<vector<string>> res;
+        unordered_map<string, int> um;
+        for (auto str: strs)
+        {
+            tmp = str;
+            sort(tmp.begin(), tmp.end());
+            if (um[tmp]>0)                      
+            {
+                res[um[tmp]-1].push_back(str);
+            }
+            else
+            {
+                res.push_back({str});
+                um[tmp] = res.size();          
+            }
+            
+        }
+                
+        return res;
+    }
+};
