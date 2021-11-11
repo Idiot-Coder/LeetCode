af4b03304241cb7fc5d6faaed9d3dac3f374a682
Aryan <90371726+Idiot-Coder@users.noreply.github.com>
2021-11-11 06:40:29 +0530

    83

---
+++ Zigzag Conversion.cpp
@@ -0,0 +1,45 @@
+//Program to Zigzag Conversion
+//Created by Aryan
+//Created on 11/11/2021
+
+class Solution {
+public:
+    string convert(string s, int nRows) {
+        if(nRows==1)
+        return s;
+    
+    int y=0;
+    bool flag= true;
+    string sArray[nRows];
+    for(int i=0;i<nRows;i++)
+        sArray[i]="";
+    
+    for(int i=0;i<s.length();i++){
+        
+        sArray[y]+=s[i];
+        
+        if(y==0){
+            flag=true;
+        }
+        if(y==nRows-1){
+            flag=false;
+        }
+        
+        if(flag==true){
+            y++;
+        }else{
+            y--;
+        }
+        
+
+    }
+
+    string ret="";
+    
+    for(int i=0;i<nRows;i++){
+        ret+=sArray[i];
+    }
+    
+    return ret;
+   }
+};
