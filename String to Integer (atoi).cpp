2b9cb0318593ed1cfaf2577c69b1d68b858caedb
Aryan <90371726+Idiot-Coder@users.noreply.github.com>
2021-12-04 14:45:45 +0530

    106

---
+++ String to Integer (atoi).cpp
@@ -0,0 +1,47 @@
+//Program to String to Integer (atoi)
+//Created by Aryan
+//Created on 04/12/2021
+
+class Solution
+{
+public:
+    int myAtoi(string str)
+    {
+        if(str == "")
+            return 0;
+        int i = 0;
+        long long res = 0;
+        while(str[i] != '\0' && str[i] == ' ')
+        {
+            i ++;
+        }
+        int flag = 1;
+        if(str[i] == '-')
+        {
+            flag = -1;
+            i ++;
+        }
+        else if(str[i] == '+')
+        {
+            i ++;
+        }
+        while(str[i] != '\0')
+        {
+            if(str[i] >= '0' &&str[i] <= '9')
+            {
+                res = res*10 + str[i] - '0';
+                if(res > INT_MAX)
+                {
+                    if(flag == 1)
+                        return INT_MAX;
+                    else
+                        return INT_MIN;
+                }
+                i ++;
+            }
+            else break;
+        }
+        res = flag * res;
+        return (int)res;
+    }
+};
